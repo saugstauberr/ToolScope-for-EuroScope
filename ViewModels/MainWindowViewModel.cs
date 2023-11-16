fc3d2fba08274330ef8ba14767d10bfa34bcb577
Martin <martinplays.839@gmail.com>
2023-11-16 03:04:13 +0100

    Create base for MVVM and base design

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolScope.WPF;
+
+public partial class MainWindowViewModel : ObservableObject
+{
+    [ObservableProperty]
+    public int width = 935;
+
+    [ObservableProperty]
+    public int height = 486;
+}
---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Shell;
+using ToolScope.WPF;
+
+namespace ToolScope.WPF
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            DataContext = new MainWindowViewModel();
+            var vm = (MainWindowViewModel)DataContext;
+
+            InitializeComponent();
+            WindowChrome.SetWindowChrome(this, new WindowChrome());
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+
+        private void CloseCommand(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
