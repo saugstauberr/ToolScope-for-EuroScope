cef3e36dfc743124735bd68841a454893ed29ded
Martin <martinplays.839@gmail.com>
2024-01-13 18:07:13 +0100

    Add dashboard base layout

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace ToolScope.WPF;
 
@@ -14,4 +16,13 @@ public partial class MainWindowViewModel : ObservableObject
 
     [ObservableProperty]
     public int height = 486;
+
+    [ObservableProperty]
+    public int currentTab = 0;
+
+    [RelayCommand]
+    public void ChangeTab(object tab)
+    {
+        CurrentTab = int.Parse(tab.ToString()!);
+    }
 }
---
+++ ViewModels/SettingsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolScope.WPF;
+
+public partial class SettingsViewModel : ObservableObject
+{
+}
