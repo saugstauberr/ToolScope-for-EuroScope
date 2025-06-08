5f0eab21b3f15d9133e29252caf8eee591be536a
Martin <57033944+saugstauberr@users.noreply.github.com>
2025-06-08 21:43:24 +0200

    Implement custom controlbar without functionality

--- ToolScope.WPF/ViewModels/MainWindowViewModel.cs
+++ ToolScope.WPF/ViewModels/MainWindowViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using Avalonia.Controls;
 using ReactiveUI;
 using Tmds.DBus.Protocol;
 using ToolScope.WPF.Models.Web;
@@ -11,13 +12,28 @@ namespace ToolScope.WPF.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    // if set print in console
     private string _currentTab = "Home";
 
     public string CurrentTab
     {
         get => _currentTab;
         set => this.RaiseAndSetIfChanged(ref _currentTab, TabIndexToString(value));
+    }
+    
+    private string _currentWindowState = "Normal";
+    
+    public string CurrentWindowState
+    {
+        get => _currentWindowState;
+        set => this.RaiseAndSetIfChanged(ref _currentWindowState, value);
+    }
+
+    private string _currentWindowStateIcon = "Expand";
+    
+    public string CurrentWindowStateIcon
+    {
+        get => _currentWindowStateIcon;
+        set => this.RaiseAndSetIfChanged(ref _currentWindowStateIcon, value);
     }
     
     private string TabIndexToString(string index)
@@ -29,5 +45,22 @@ public partial class MainWindowViewModel : ViewModelBase
             "2" => "Settings",
             _ => throw new ArgumentOutOfRangeException(nameof(index), "Invalid tab index")
         };
+    }
+    
+    public void MinimizeWindow(object obj)
+    {
+        if (obj is Window window) { window.Hide();}
+    }
+    
+    public void ToggleWindowState()
+    {
+        CurrentWindowStateIcon = CurrentWindowStateIcon == "Expand" ? "Compress" : "Expand";
+        CurrentWindowState = CurrentWindowState == "Maximize" ? "Normal" : "Maximize";
+        
+    }
+
+    public void CloseWindow(object obj)
+    {
+        if (obj is Window window) { window.Close();}
     }
 }
