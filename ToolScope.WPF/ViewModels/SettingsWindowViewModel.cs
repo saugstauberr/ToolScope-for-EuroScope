fb63744cb2b6a8c4fbaaa945b379f518fd4b9c5b
Martin <57033944+saugstauberr@users.noreply.github.com>
2025-06-26 01:45:06 +0200

    Make settings savable and loadable

--- ToolScope.WPF/ViewModels/SettingsWindowViewModel.cs
+++ ToolScope.WPF/ViewModels/SettingsWindowViewModel.cs
@@ -1,17 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using DynamicData;
+using ReactiveUI;
+using ToolScope.WPF.Models;
 using ToolScope.WPF.Models.Web;
 
 namespace ToolScope.WPF.ViewModels;
 
-public partial class SettingsWindowViewModel : ViewModelBase, INotifyPropertyChanged
+public partial class SettingsWindowViewModel : ViewModelBase
 {
-    public new event PropertyChangedEventHandler? PropertyChanged;
+    private string[] _keyNames = ["RealName", "Cid", "CallSign", "Password", "HoppieCode", "EuroScopeFolder"];
+    private string[] _inputValues = new string [100];
 
-    private new void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    public SettingsWindowViewModel()
     {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        // Initialize the input values with existing configuration values
+        for (var i = 0; i < _keyNames.Length; i++)
+        {
+            var key = _keyNames[i];
+            InputValues[i] = ConfigHandler.Get(key);
+        }
+    }
+    
+    public string[] InputValues
+    {
+        get => _inputValues;
+        set => this.RaiseAndSetIfChanged(ref _inputValues, value);
+    }
+
+    public void SaveInput()
+    {
+        for (var i = 0; i < _keyNames.Length; i++)
+        {
+            var key = _keyNames[i];
+            var value = InputValues[i];
+
+            if (string.IsNullOrEmpty(value))
+            {
+                continue; // Skip empty values
+            }
+
+            // Set the configuration value using the ConfigHandler
+            ConfigHandler.Set(key, value);
+        }
     }
 }
