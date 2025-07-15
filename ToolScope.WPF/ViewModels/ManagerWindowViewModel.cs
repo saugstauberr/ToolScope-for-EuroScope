e8b54c3fa55507b968b554c5b01cd1f3e7b19fad
Martin <57033944+saugstauberr@users.noreply.github.com>
2025-07-16 01:12:38 +0200

    Add AddPackageWindow and AddPackageButton

--- ToolScope.WPF/ViewModels/ManagerWindowViewModel.cs
+++ ToolScope.WPF/ViewModels/ManagerWindowViewModel.cs
@@ -4,10 +4,16 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
 using CommunityToolkit.Mvvm.ComponentModel;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
 using ReactiveUI;
 using ToolScope.WPF.Models;
 using ToolScope.WPF.Models.Web;
+using ToolScope.WPF.Views;
 
 namespace ToolScope.WPF.ViewModels;
 
@@ -24,5 +30,23 @@ public partial class ManagerWindowViewModel : ViewModelBase
             new PackageClass("EDXX", "EDDM", "Test-Variant")
         };
         InstalledPackages = new ObservableCollection<PackageClass>(people);
+    }
+
+    public async void OpenAddPackageWindow()
+    {
+        if (ConfigHandler.Get("EuroScopeFolder") == "")
+        {
+            var box = MessageBoxManager
+                .GetMessageBoxStandard("Warning", "Please set a folder for new packages first!");
+            var result = await box.ShowAsync();
+            return;
+        }
+        
+        var window = new Window
+        {
+            Title = "ToolScope - Adding new package",
+            Content = new AddPackageWindow()
+        };
+        window.Show();
     }
 }
---
+++ ToolScope.WPF/Views/AddPackageWindow.axaml.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using ToolScope.WPF.ViewModels;
+
+namespace ToolScope.WPF.Views;
+
+public partial class AddPackageWindow : UserControl
+{
+    public AddPackageWindow()
+    {
+        InitializeComponent();
+        DataContext = new AddPackageWindowViewModel();
+        
+    }
+}
